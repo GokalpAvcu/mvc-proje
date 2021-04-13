ae587e2eece9ca5e954d31c7de9e7b4c751a2cf9
Gökalp Avcu <avcu219@gmail.com>
2021-04-13 14:04:22 +0300

    13 Nisan

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,17 @@ namespace EntityLayer.Concrete
 {
     public class Content
     {
+        [Key]
         public int ContentID { get; set; }
+
+        [StringLength(1000)]
         public string ContentValue { get; set; }
         public DateTime ContentDate { get; set; }
-        //ContentYazar
-        //ContentBaşlık
+
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
     }
 }
