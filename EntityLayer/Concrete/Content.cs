2395ff77310bcb1a403c3555ce503c288517ddb0
Gökalp Avcu <avcu219@gmail.com>
2021-04-17 03:01:24 +0300

    17April

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -19,7 +19,7 @@ namespace EntityLayer.Concrete
         public int HeadingID { get; set; }
         public virtual Heading Heading { get; set; }
 
-        public int WriterID { get; set; }
+        public int? WriterID { get; set; }
         public virtual Writer Writer { get; set; }
     }
 }
