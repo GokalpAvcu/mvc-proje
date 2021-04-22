e9919a5a7706203fbe2ebe543754662ed8e93bbc
Gökalp Avcu <avcu219@gmail.com>
2021-04-22 04:37:35 +0300

    April22

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+   public interface ICategoryDal
+    {
+        //CRUD
+        //Type Name();
+        List<Category> List();
+
+        void Insert(Category p);
+
+        void Update(Category p);
+
+        void Delete(Category p);
+        
+    }
+}
---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+   public interface IRepository<T>
+    {
+        List<T> List();
+
+        void Insert(T p);
+
+        void Delete(T p);
+
+        void Update(T p);
+    }
+}
