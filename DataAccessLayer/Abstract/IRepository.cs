adff06d50cdeb4dc399508152153a0362491866a
Gökalp Avcu <avcu219@gmail.com>
2021-04-25 03:36:28 +0300

    Ap25

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,5 +16,7 @@ namespace DataAccessLayer.Abstract
         void Delete(T p);
 
         void Update(T p);
+
+        List<T> List(Expression<Func<T, bool>> filter);
     }
 }
--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -1,5 +1,4 @@
 ﻿using DataAccessLayer.Abstract;
-using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,42 +9,46 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.Repositories
 {
-    public class CategoryRepository : ICategoryDal
+    public class GenericRepository<T> : IRepository<T> where T : class
     {
         Context c = new Context();
-        DbSet<Category>_object;
-        public void Delete(Category p)
+
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = c.Set<T>();
+        }
+
+
+        public void Delete(T p)
         {
             _object.Remove(p);
             c.SaveChanges();
         }
 
-        public void Insert(Category p)
+        public void Insert(T p)
         {
             _object.Add(p);
             c.SaveChanges();
         }
 
-        public List<Category> List()
+        public List<T> List()
         {
             return _object.ToList();
+            
         }
 
-        public List<Category> List(Expression<Func<Category, bool>> filter)
+        public List<T> List(Expression<Func<T, bool>> filter)
         {
-            throw new NotImplementedException();
+            return _object.Where(filter).ToList();
         }
 
-        public void Update(Category p)
+        public void Update(T p)
         {
             c.SaveChanges();
         }
     }
-}
 
-/* 
-  ToList
-  Add
-  Remove
-  Find
-  */
+
+}
