94d4c7c7bbe3a043a3b9957cc5f01150893c8e3e
Gökalp Avcu <avcu219@gmail.com>
2021-04-27 16:54:38 +0300

    Ap27

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -18,19 +18,20 @@ namespace BusinessLayer.Concrete
         }
         public void CategoryAddBL(Category p)
         {
-            if(p.CategoryName=="" || p.CategoryName.Length<=3 ||
-                    p.CategoryDescription== "" || p.CategoryName.Length>=51)
-            
-                {
-                    //hata mesajı
-                }
-            else
-            {
-                repo.Insert(p);
-            }
-               
+            repo.Insert(p);
+            //if(p.CategoryName=="" || p.CategoryName.Length<=3 ||
+            //  p.CategoryDescription== "" || p.CategoryName.Length>=51)
 
-            
+            // {
+            //     //hata mesajı
+            //}
+            //else
+            //{
+
+            // }
+
+
+
         }
 
     }
--- MvcProje/Controllers/CategoryController.cs
+++ MvcProje/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,23 @@ namespace MvcProje.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager();
+        
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();
+            return View(categoryvalues);
+        }
+        public ActionResult AddCategory(Category p) 
+        {
+            cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
+        
+        
         }
     }
 }
