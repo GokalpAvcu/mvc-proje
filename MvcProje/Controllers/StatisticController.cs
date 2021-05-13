4f6c05f89784dc01b062b0b98da4369d2ed80630
Gökalp Avcu <avcu219@gmail.com>
2021-05-13 18:02:20 +0300

    13May

---
+++ MvcProje/Controllers/StatisticController.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class StatisticController : Controller
+    {
+        // GET: Statistic
+
+        Context _context = new Context();
+        public ActionResult Index()
+        {
+            var totalCategory = _context.Categories.Count(); //Toplam Kategori Sayisi
+            ViewBag.totalNumberOfCategories = totalCategory;
+
+            var softwareCategory = _context.Headings.Count(x => x.CategoryID == 8); //Yazilim Kategorisi (8) baslik sayisi
+            ViewBag.softwareCategoryTitleNumber = softwareCategory;
+
+            var writerNameSortByA = _context.Writers.Count(x => x.WriterName.Contains("a")); // Yazar adinda "a" harfi gecen yazar sayisi
+            ViewBag.writerNameSortByA = writerNameSortByA;
+
+            var mostTitles = _context.Headings.Max(x => x.Category.CategoryName); // En fazla basliga sahip kategori adi
+            ViewBag.categoryNameWithTheMostTitles = mostTitles;
+
+            var categoryStatusTrue = _context.Categories.Count(x => x.CategoryStatus == true); // Kategoriler tablosundaki aktif kategori sayisi
+            ViewBag.activeCategories = categoryStatusTrue;
+            return View();
+        }
+    }
+}
