e9cec562d83313be1ee229ffe30f41df4eec1cce
Gökalp Avcu <avcu219@gmail.com>
2021-05-30 20:10:06 +0300

    30May

--- DataAccessLayer/EntityFramework/EfAboutDal.cs
+++ DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfAboutDal: GenericRepository<Category>, ICategoryDal
+    public class EfAboutDal: GenericRepository<About>, IAboutDal
     {
     }
 }
