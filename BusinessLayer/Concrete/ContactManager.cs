cc6af1d979fe3b067810fb2593d62af9919f90c8
Gökalp Avcu <avcu219@gmail.com>
2021-06-01 18:27:09 +0300

    1June

--- BusinessLayer/Concrete/ContactManager.cs
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class ContactManager
+    public class ContactManager : IContactService
     {
+        IContactDal _Contactdal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _Contactdal = contactDal;
+        }
+
+        public void ContactAdd(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ContactDelete(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ContactUpdate(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> GetList()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
