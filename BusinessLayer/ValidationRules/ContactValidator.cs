7e17636f5f7e242cb9e7b61c513945c15e75fe5c
Gökalp Avcu <avcu219@gmail.com>
2021-06-01 19:26:36 +0300

    1June

--- BusinessLayer/ValidationRules/ContactValidator.cs
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -17,7 +17,7 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı adını boş geçemezsiniz");
             RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz");
             RuleFor(x => x.UserName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz");
-            RuleFor(x => x.Subject).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın");
+            RuleFor(x => x.Subject).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayın");
         }
     }
 }
