5fb0a2635d844e8a9c424d0e4043b9846265373a
Gökalp Avcu <avcu219@gmail.com>
2021-06-04 01:16:23 +0300

    4June

--- MvcProje/Controllers/MessageController.cs
+++ MvcProje/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace MvcProje.Controllers
     public class MessageController : Controller
     {
         // GET: Message
+
+        MessageManager cm = new MessageManager(new EfMessageDal());
         public ActionResult Inbox()
         {
             return View();
