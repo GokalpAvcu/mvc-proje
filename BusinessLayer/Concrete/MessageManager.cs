b50786c31e526b2de2c4d1cfd183e524af3fa2a1
Gökalp Avcu <avcu219@gmail.com>
2021-06-04 00:57:01 +0300

    4June

--- BusinessLayer/Concrete/MessageManager.cs
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +12,30 @@ namespace BusinessLayer.Concrete
    public class MessageManager: IMessageService
     {
         IMessageDal messageDal;
+
+        public Message GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageAdd(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageDelete(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageUpdate(Message message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
