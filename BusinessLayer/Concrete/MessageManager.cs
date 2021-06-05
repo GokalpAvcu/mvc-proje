60405c1304422907e2f274d5ff37a448d8ac9688
Gökalp Avcu <avcu219@gmail.com>
2021-06-05 11:18:53 +0300

    5June

--- BusinessLayer/Concrete/MessageManager.cs
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -30,7 +30,7 @@ namespace BusinessLayer.Concrete
 
         public List<Message> GetListSendbox()
         {
-           return _messageDal.List(x=> x.SenderMail == "admin")
+            return _messageDal.List(x => x.SenderMail == "admin@gmail.com");
         }
 
         public void MessageAdd(Message message)
