6c7164970bb15a8a93b6bf97afed0b878253ca60
Gökalp Avcu <avcu219@gmail.com>
2021-06-06 17:40:17 +0300

    6June

--- BusinessLayer/ValidationRules/MessageValidator.cs
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules
 {
-    class MessageValidator
+    class MessageValidator: AbstractValidator<Message>
     {
+        public MessageValidator()
     }
 }
