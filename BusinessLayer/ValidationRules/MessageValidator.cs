cd164187c6b42557ce6b4ee8acabd8df7ca9b896
Gökalp Avcu <avcu219@gmail.com>
2021-06-06 18:00:00 +0300

    6June

--- BusinessLayer/ValidationRules/MessageValidator.cs
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -15,9 +15,8 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı adresini boş geçemezsiniz");
             RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu adresini boş geçemezsiniz");
             RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı boş geçemezsiniz");
-            RuleFor(x => x.ReceiverMail).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz");
             RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz");
-            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen 100 karakterden fazla değer girişi yapmayın");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("Lütfen 100 karakterden fazla değer girişi yapmayın");
         }
     }
 }
