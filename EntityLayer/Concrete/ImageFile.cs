eded8401e432196fea4680b696d6260dfca50fd8
Gökalp Avcu <avcu219@gmail.com>
2021-06-11 00:06:06 +0300

    11June

--- EntityLayer/Concrete/ImageFile.cs
+++ EntityLayer/Concrete/ImageFile.cs
@@ -8,9 +8,11 @@ namespace EntityLayer.Concrete
 {
    public class ImageFile
     {
+        [Key]
         public int ImageID { get; set; }
+        [StringLength(100)]
         public string ImageName { get; set; }
-
+        [StringLength(250)]
         public string ImagePath { get; set; }
     }
 }
