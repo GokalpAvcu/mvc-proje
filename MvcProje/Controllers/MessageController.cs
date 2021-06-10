bec985c21dc5066eb548dedae74713229c8219ff
Gökalp Avcu <avcu219@gmail.com>
2021-06-10 13:12:54 +0300

    10June

--- MvcProje/Controllers/MessageController.cs
+++ MvcProje/Controllers/MessageController.cs
@@ -2,6 +2,7 @@
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,27 +15,27 @@ namespace MvcProje.Controllers
     {
         // GET: Message
 
-        MessageManager cm = new MessageManager(new EfMessageDal());
+        MessageManager mm = new MessageManager(new EfMessageDal());
         MessageValidator messagevalidator = new MessageValidator();
         public ActionResult Inbox()
         {
-            var messagelist = cm.GetListInbox();
+            var messagelist = mm.GetListInbox();
             return View(messagelist);
         }
         public ActionResult Sendbox()
         {
-            var messagelist = cm.GetListSendbox();
+            var messagelist = mm.GetListSendbox();
             return View(messagelist);
         }
 
         public ActionResult GetInBoxMessageDetails(int id)
         {
-            var values = cm.GetByID(id);
+            var values = mm.GetByID(id);
             return View(values);
         }
         public ActionResult GetSendBoxMessageDetails(int id)
         {
-            var values = cm.GetByID(id);
+            var values = mm.GetByID(id);
             return View(values);
         }
 
@@ -47,6 +48,19 @@ namespace MvcProje.Controllers
         [HttpPost]
         public ActionResult NewMessage(Message p)
         {
+            ValidationResult results = messagevalidator.Validate(p);
+            if (results.IsValid)
+            {
+                mm.MessageAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
