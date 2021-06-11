232f8d750822a55ec86d6c3857c3711350cad5e1
Gökalp Avcu <avcu219@gmail.com>
2021-06-11 15:49:38 +0300

    11June

--- BusinessLayer/Concrete/ImageFileManager.cs
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class ImageFileManager
+    public class ImageFileManager : IImageFileService
     {
+        IImageFileDal _imagefileDal;
+
+        public ImageFileManager(IImageFileDal imagefileDal)
+        {
+            _imagefileDal = imagefileDal;
+        }
     }
 }
