e374cf4a3173ef34e25cbc413aa52153f543ef26
Gökalp Avcu <avcu219@gmail.com>
2021-06-11 15:51:54 +0300

    11June

--- MvcProje/Controllers/GalleryController.cs
+++ MvcProje/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace MvcProje.Controllers
     public class GalleryController : Controller
     {
         // GET: Gallery
+        ImageFileManager hm = new ImageFileManager(new EfHeadingDal());
+
         public ActionResult Index()
         {
             return View();
