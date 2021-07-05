d8f2324af3db34f203f1766bd0c9fda23f722dc7
Gökalp Avcu <avcu219@gmail.com>
2021-07-05 16:47:05 +0300

    5July

--- MvcProje/Controllers/WriterPanelMessageController.cs
+++ MvcProje/Controllers/WriterPanelMessageController.cs
@@ -38,5 +38,31 @@ namespace MvcProje.Controllers
             var values = mm.GetByID(id);
             return View(values);
         }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            ValidationResult results = messagevalidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("SendBox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
     }
 }
